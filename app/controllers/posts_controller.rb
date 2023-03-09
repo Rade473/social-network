5c0aa8fed5d6689e8352d571bc1a5e3062c1972c
Rade473 <r.shekularats@gmail.com>
2023-03-09 18:00:33 +0100

    Posts model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @post}
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.equire(:post).permit(:body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  resources :posts
+
   get 'friends', to: 'pages#friends'
 end
