d617247f913183d22cb716b3ad0435360fccd981
Rade473 <r.shekularats@gmail.com>
2023-03-17 22:19:21 +0100

    Comment broadcasting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  after_create_commit -> { broadcast_prepend_to "comments", partial: "comments/comment", locals: { comment: self }, target: "comments_for_post_#{self.post.id}" }
 end
